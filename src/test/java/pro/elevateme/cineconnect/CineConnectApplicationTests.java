d20e4ed878c3681f185b417f0230b736e6d9080a
Pinky Sodhi <pinky.sodhi@gmail.com>
2026-01-29 15:11:51 -0500

    initial commit

---
+++ src/test/java/pro/elevateme/cineconnect/CineConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.elevateme.cineconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
