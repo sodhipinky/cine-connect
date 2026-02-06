c52800d3a67dc24c9a2688485746daa37e70f0d4
Pinky Sodhi <pinky.sodhi@gmail.com>
2026-02-06 04:49:45 -0500

    add GraphQlMovieController with CRUD operations for Movie entity and define GraphQL schema

--- src/main/java/pro/elevateme/cineconnect/controller/GraphQlMovieController.java
+++ src/main/java/pro/elevateme/cineconnect/controller/GraphQlMovieController.java
@@ -1,29 +1,41 @@
 package pro.elevateme.cineconnect.controller;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 import pro.elevateme.cineconnect.model.Movie;
+import pro.elevateme.cineconnect.service.MovieService;
 
 import java.util.List;
 import java.util.UUID;
 
-public class GraphQlMovieController implements MovieController {
-    @Override
-    public Movie create(Movie movie) {
-        return null;
+@Controller
+public class GraphQlMovieController {
+    private final MovieService movieService;
+
+    public GraphQlMovieController(MovieService movieService) {
+        this.movieService = movieService;
     }
 
-    @Override
-    public ResponseEntity<Movie> getById(UUID movieId) {
-        return null;
+    @MutationMapping
+    public Movie createMovie(@Argument Movie movie) {
+        return movieService.save(movie);
     }
 
-    @Override
-    public List<Movie> getAll() {
-        return List.of();
+    @QueryMapping
+    public Movie getMovie(@Argument UUID movieId) {
+        return movieService.findById(movieId).orElse(null);
     }
 
-    @Override
-    public ResponseEntity<Void> delete(UUID movieId) {
-        return null;
+    @QueryMapping
+    public List<Movie> getMovies() {
+        return movieService.findAll();
+    }
+
+    @MutationMapping
+    public Boolean deleteMovie(@Argument UUID movieId) {
+        movieService.deleteById(movieId);
+        return true;
     }
 }
